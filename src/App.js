dfc410edcd3ab9e78a6e4731683a6ddc9fea7a5b
neilbenjamin <neilbenjaminmusic@gmail.com>
2023-09-03 12:43:04 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+//Main app that exports to index.js This is the app component that all other compnent pages beed to be exported to and
+// imported on for exporting to index.js.
+
 import './App.css';
 import UserObjectComponent from './userObject.js';
 
--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -1,10 +1,14 @@
+//This was an amazing project to learn. Lot's of guidance from VSC, Hyperion Node Material, React and OpenAI for updated 
+//Installation methods and terminal commands. This was really amazing to learn. Wow. There is a link to gitHub for this project aswell.
 import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 // Define the function component
 function ShoppingCartComponent() {
+  //Shopping list array
   const shoppingList = ["Apple", "Pear", "Lemon", "Melon", "Grapefruit", "Banana"];
+  //React Bootstrap (BS for shorthand) accordian
   const [showList, setShowList] = useState(false);
 
   // Toggle the state to show/hide the list
